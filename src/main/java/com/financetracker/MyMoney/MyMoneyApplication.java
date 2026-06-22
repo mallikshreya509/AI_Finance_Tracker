8e4ac5889a9acf7955460aa44a9e1af511b7fcde
Shreya Mallik <272812472+mallikshreya509@users.noreply.github.com>
2026-06-22 10:45:25 +0530

    Initial commit

---
+++ src/main/java/com/financetracker/MyMoney/MyMoneyApplication.java
@@ -0,0 +1,14 @@
+package com.financetracker.MyMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyApplication.class, args);
+		System.out.println("MyMoneyApplication started successfully!");
+	}
+
+}
---
+++ src/test/java/com/financetracker/MyMoney/MyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.MyMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
